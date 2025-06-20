567dadbd4658b27a4d36c0cb0c589706f8f31c67
moataz <moataz.tahri1@gmail.com>
2025-06-20 22:10:43 +0100

    [Feat]-Added JWT token to login request. [Fix]-Email already taken method fixed.

--- backend/src/main/java/com/example/backend/controllers/user/AuthenticationController.java
+++ backend/src/main/java/com/example/backend/controllers/user/AuthenticationController.java
@@ -1,7 +1,9 @@
 package com.example.backend.controllers.user;
 
+import com.example.backend.exceptions.UserNotFoundException;
 import com.example.backend.models.user.AuthenticationRequest;
 import com.example.backend.models.user.UserDto;
+import com.example.backend.services.auth.AuthenticationService;
 import com.example.backend.services.user.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -11,29 +13,45 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/auth")
 @AllArgsConstructor
 public class AuthenticationController {
 
     private final UserService userService;
+    private final AuthenticationService authService;
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody UserDto userDto) {
-        if (userService.getUserByEmail(userDto.getEmail()) != null) {
+        // If the user is new, register will be triggered
+        try {
+            userService.getUserByEmail(userDto.getEmail());
             return ResponseEntity.badRequest().body("Email is already taken");
+        } catch (UserNotFoundException e) { // No email taken
+            userService.register(userDto);
+            return ResponseEntity.ok("User registered successfully");
         }
-        userService.register(userDto);
-        return ResponseEntity.ok("User registered successfully");
     }
 
     @PostMapping("/login")
     public ResponseEntity<Object> login(@Valid @RequestBody AuthenticationRequest request) {
-        if (userService.login(request.getEmail(), request.getPassword())) {
-            return ResponseEntity.ok(userService.getUserByEmail(request.getEmail()));
-        } else {
-            return ResponseEntity.badRequest().body("Email or password is incorrect");
+        boolean isAuthenticated = authService.authenticate(request.getEmail(), request.getPassword());
+        long expirationTime = 60 * 60 * 1000; // 1 hour
+        if (isAuthenticated) {
+            Map<String, Object> userInfo = new HashMap<>();
+            UserDto userDto = userService.getUserByEmail(request.getEmail());
+            userInfo.put("email", userDto.getEmail());
+            userInfo.put("role",  userDto.getRole().name());
+            String token = authService.generateToken(userInfo, expirationTime);
+            return ResponseEntity.ok(Map.of("token", token));
         }
+        return ResponseEntity.badRequest().body("Invalid email or password");
     }
+
+
+
 
 }
