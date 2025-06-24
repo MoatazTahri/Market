99ecc5f1d57f6fbf6000fff14b0a933ebc43c963
moataz <moataz.tahri1@gmail.com>
2025-06-24 19:27:01 +0100

    [Refactor]-Changed axios direct call to axiosInstance paths filter.

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,15 +1,5 @@
-import axios from "axios";
-import Cookies from "js-cookie";
-
-const API_URL = import.meta.env.VITE_API_URL
-
-// Send one global header for all secured paths
-const api = axios.create({
-    headers: {
-        Authorization: `Bearer ${Cookies.get("token")}`
-    }
-})
+import axiosInstance from "./axiosInstance.js";
 
 export const getUserByEmail = (email) => {
-    return api.get(API_URL + "/user/find", {params: {email: email}})
+    return axiosInstance.get("/user/find", {params: {email: email}})
 }
