867c94b0ae3d0f0301a98a08bb9cd8571b6999ce
moataz <moataz.tahri1@gmail.com>
2025-06-24 02:43:04 +0100

    [Feat]-Added method to retrieve products by their seller to repository

--- backend/src/main/java/com/example/backend/repositories/ProductRepository.java
+++ backend/src/main/java/com/example/backend/repositories/ProductRepository.java
@@ -4,6 +4,9 @@ import com.example.backend.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllBySellerId(Long sellerId);
 }
