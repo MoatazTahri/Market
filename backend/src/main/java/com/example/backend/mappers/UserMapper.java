39488b11cfb744701fbc9ce9b7461b21d1e49308
moataz <moataz.tahri1@gmail.com>
2025-06-24 02:55:42 +0100

    [Refactor]-Added refresh token to UserMapper and changed ProductDto to ProductResponse.

--- backend/src/main/java/com/example/backend/mappers/UserMapper.java
+++ backend/src/main/java/com/example/backend/mappers/UserMapper.java
@@ -2,7 +2,7 @@ package com.example.backend.mappers;
 
 import com.example.backend.entities.Product;
 import com.example.backend.entities.User;
-import com.example.backend.models.product.ProductDto;
+import com.example.backend.models.product.ProductResponse;
 import com.example.backend.models.user.UserDto;
 
 import java.util.HashSet;
@@ -10,17 +10,19 @@ import java.util.Set;
 
 public class UserMapper {
     public static UserDto toUserDto(User user) {
-        Set<ProductDto> productsDto = new HashSet<>();
+        Set<ProductResponse> productsDto = new HashSet<>();
         if (user.getProducts() != null) {
             for (Product product : user.getProducts()) {
-                productsDto.add(ProductMapper.toProductDto(product));
+                productsDto.add(ProductMapper.toProductResponse(product));
             }
         }
         return UserDto.builder()
+                .id(user.getId())
                 .email(user.getEmail())
                 .firstName(user.getFirstName())
                 .lastName(user.getLastName())
                 .role(user.getRole())
+                .refreshToken(user.getRefreshToken())
                 .locked(user.isLocked())
                 .expired(user.isExpired())
                 .active(user.isActive())
